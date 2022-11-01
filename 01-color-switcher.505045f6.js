72ca9f9f8f718cc1da60daa01740f9f9b79170cc
MishinaIra55 <93260207+MishinaIra55@users.noreply.github.com>
2022-11-01 08:57:22 +0000

    Deploying to gh-pages from @ MishinaIra55/goit-js-hw-09@bd94083917b02f68724a9a45b0e4651bd61c2aeb 🚀

---
+++ 01-color-switcher.505045f6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");let e=null;t.addEventListener("click",(()=>{e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));
+//# sourceMappingURL=01-color-switcher.505045f6.js.map
