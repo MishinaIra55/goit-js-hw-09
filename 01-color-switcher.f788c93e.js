843f404722bc6c1b52dc693d56cc8d52e3d06b6d
MishinaIra55 <93260207+MishinaIra55@users.noreply.github.com>
2022-11-01 08:59:45 +0000

    Deploying to gh-pages from @ MishinaIra55/goit-js-hw-09@3ab226f70e0b91dde4cb22b774f55dc7ca4fe828 🚀

--- 01-color-switcher.f788c93e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");t.addEventListener("click",(function(){setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.f788c93e.js.map
