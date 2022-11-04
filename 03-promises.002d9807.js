1bd3cd3374637ab83d3a73abcb9a465dff8c7dfc
MishinaIra55 <93260207+MishinaIra55@users.noreply.github.com>
2022-11-04 13:45:57 +0000

    Deploying to gh-pages from @ MishinaIra55/goit-js-hw-09@40611cb649f5c969080960adf14d49e5e3d13783 🚀

---
+++ 03-promises.002d9807.js
@@ -0,0 +1,2 @@
+!function(){var n=document.getElementById("button");console.log(n)}();
+//# sourceMappingURL=03-promises.002d9807.js.map
---
+++ 03-promises.7341163b.js
@@ -0,0 +1,2 @@
+const o=document.getElementById("button");console.log(o);
+//# sourceMappingURL=03-promises.7341163b.js.map
