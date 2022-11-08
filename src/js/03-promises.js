6ee1901b2ab04605a6cfe1f75e4027d8e56afd51
MishynaIra <gortenzia1987@gmail.com>
2022-11-08 18:24:05 +0200

    logica последовательного добавоения

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,48 +1,56 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => submitAction(event));
+import Notiflix from 'notiflix';//подключила библиотеку
 
+const form = document.querySelector('.form');//достучалась до формы
 
+// console.log(form);
+form.addEventListener('submit', OnSubmitAction);//навешала слушателя
 
+//логика увеличения delay на step
+function OnSubmitAction(event) {
+  event.preventDefault();
 
+  const amount = document.querySelector('input[name = "amount"]').value;
+  const delay = document.querySelector('input[name = "delay"]').value;
+  const step = document.querySelector('input[name = "step"]').value;
+
+  for (let i = 1; i <= amount; i+= 1) {
+  
+    let numberdelay = Number(delay);
+    let numberstep = Number(step);
+    
+  setTimeout(() => {
+    console.log(i, numberdelay, numberstep);
+
+    setTimeout(() => {
+      createPromise(i, numberdelay + numberstep * i )
+      .then(onSuccess)
+      .catch(onError);
+    }, numberdelay);
+  }, numberstep);
+
+  form.reset();
+}
+}
+
+function onError(data) {
+  Notiflix.Notify.failure(data);
+}
+
+function onSuccess(data) {
+  Notiflix.Notify.success(data);
+}
+
+//создание промиса
 function createPromise(position, delay) {
-    return  new Promise((resolve, reject) => {
-      // console.log(delay);
-      
-        const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        } else {
-          reject(`❌ Rejected promise ${position} in ${delay}ms`);
-        }
-  }
-    )};
-    
-    function submitAction(event){
-      event.preventDefault();
-    
-      const amount = document.querySelector('input[name = "amount"]').value;
-      const delay = document.querySelector('input[name = "delay"]').value;
-      const step = document.querySelector('input[name = "step"]').value;
-      
-    
-      for(let i = 1; i <= amount; i++) {
-        console.log('help');
-        
-          createPromise(i, delay).then((resolve) => {
-            console.log('result => ', resolve);
-          }).catch((reject) => {
-            console.log('result => ', reject);
-          })
-        
-        
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
-    };
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+      
+    }, delay);
+  });
+}
